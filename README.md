b34f3b2085949715a5aa9b2f5d6a3471c4e60d81
barboza gonzalo <barbozagonzaloagustin8@gmail.com>
2023-06-30 20:33:14 -0300

    se configuraron los meddlewares

---
+++ README.md
@@ -0,0 +1,3 @@
+paso 1 : abrimos la consola y ejecutamos el comando npm install.
+paso 2 : ejecutamos el comando npm i sequelize mysql2 dotenv.
+paso 3 : en el archivo package.json en script agregamos el dev.
--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 // Imports
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const express = require('express');
+
 
 const path = require('path');
 
 const app = express();
 
+require('dotenv').config();
+const port = process.env.PORT || 3000
+
+
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.get("*", (req, res) => {
+    res.status(404).send("error 404")
+})
+
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on port ${port}`));
