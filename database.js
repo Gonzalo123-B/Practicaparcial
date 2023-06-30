b512e1ee93b0a1cfbd939c529bf6cc2f6bb1db72
barboza gonzalo <barbozagonzaloagustin8@gmail.com>
2023-06-30 20:47:54 -0300

    se agrego database

---
+++ database.js
@@ -0,0 +1,14 @@
+const { Sequelize, Model } = require ("sequelize")
+require ("dotenv").config()
+
+const {DB_NAME,DB_USER,DB_HOST,DB_PORT,DB_DIALECT } = process.env
+
+const sequelize = new Sequelize(DB_Name,DB_USER, "", {
+host: DB_HOST
+dialect: DB_DIALECT
+
+})
+
+
+module.exports = {sequelize, Model}
+
